9a622c029169fef935e2200802e9c8f4b08220e4
hamellen <zzodong0309@naver.com>
2024-09-26 19:01:14 +0900

    Add files via upload

---
+++ Engine/GeometryHelper.h
@@ -0,0 +1,12 @@
+#pragma once
+#include "Geometry.h"
+#include "VertexData.h"
+class GeometryHelper
+{
+
+public:
+
+	static void CreateQuad(shared_ptr<Geometry<VertexColorData>> geometry,Color color);
+
+};
+
